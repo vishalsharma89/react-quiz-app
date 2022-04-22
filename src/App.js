f52f953b9c40821cfdf5b94227aa62fba2a89990
vishalsharma89 <sharmavishal9643@gmail.com>
2022-04-22 17:19:50 +0530

    Created Routes for app using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
+import Quiz from './Pages/Quiz/Quiz';
+import Result from './Pages/Result/Result';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
         <Header />
+
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/quiz" exact>
+            <Quiz />
+          </Route>
+          <Route path="/result" exact>
+            <Result />
+          </Route>
+        </Switch>
       </div>
       <Footer />
     </BrowserRouter>
