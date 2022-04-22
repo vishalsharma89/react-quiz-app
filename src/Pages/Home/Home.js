b3eeae96a8fd7ae4a3ed34fb6f790cce9f13fb1b
vishalsharma89 <sharmavishal9643@gmail.com>
2022-04-22 18:11:59 +0530

    Created Home page layout

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,60 @@
+import { Button, MenuItem, TextField } from '@material-ui/core'
 import React from 'react'
+import Categories from '../../Data/Categories'
+import "./Home.css"
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='content'>
+            <div className="settings">
+                <span style={{ fontSize: 30 }}>Quiz Settings</span>
+
+                <div className="settings__select">
+                    <TextField
+                        style={{ marginBottom: 25 }}
+                        label="Enter Your Name"
+                        varient="outlined"
+                    />
+                    <TextField
+                        select
+                        style={{ marginBottom: 30 }}
+                        label="Select Category"
+                        varient="outlined"
+                    >
+                        {Categories.map((cat) => (
+                            <MenuItem key={cat.category} value={cat.value}
+                            >
+                                {cat.category}
+
+                            </MenuItem>
+
+                        ))}
+
+                    </TextField>
+                    <TextField
+                        select
+                        style={{ marginBottom: 30 }}
+                        label="Select Category"
+                        varient="outlined"
+                    >
+                        <MenuItem key="Easy" value="easy">
+                            Easy
+                        </MenuItem>
+                        <MenuItem key="Medium" value="medium">
+                            Medium
+                        </MenuItem>
+                        <MenuItem key="Hard" value="hard">
+                            Hard
+                        </MenuItem>
+                    </TextField>
+
+                    <Button variant="contained" color="primary" size="large">
+                        Start Quiz
+                    </Button>
+                </div>
+            </div>
+            <img src="/quiz.svg" className="banner" alt="quiz img" />
+        </div>
     )
 }
 
