3652a2ee7046423460a71c0d8abc9ee50ff053a7
vishalsharma89 <sharmavishal9643@gmail.com>
2022-04-22 17:06:35 +0530

    header and footer created

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div
+            style={{
+                textAlign: "center",
+                marginBottom: 10,
+            }}
+        >
+            Made by{" "}
+            <a
+                href="Vishal"
+                style={{ cursor: "pointer" }}
+            >
+                Vishal Sharma
+            </a>
+        </div>
+    )
+}
+
+export default Footer
